b266dd970c1f1b2aff2fbf4ed30ec7ed7cd3ee2d
rmunoz <richosojason@msn.com>
2021-02-22 23:08:26 -0500

    added user search component with refs

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
 
+import EventComponent from "./events/EventComponent";
+
+import UserSearchWithRef from "./refs/UserSearch";
+
 const App = () => {
   return (
     <div>
+      <EventComponent />
       <GuestList />
       <UserSearch />
+      <UserSearchWithRef />
     </div>
   );
 };
