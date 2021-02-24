86ecae203c317edb53d86d5e3527d9bb713a1451
rmunoz <richosojason@msn.com>
2021-02-23 23:32:49 -0500

    added store provider to app component

--- redux-ts/src/App.tsx
+++ redux-ts/src/App.tsx
@@ -1,5 +1,17 @@
+import { Provider } from "react-redux";
+import { store } from "./redux";
+
+import RepositoriesList from "./components/RepositoriesList";
+
 function App() {
-  return <div className="App">Redux ts</div>;
+  return (
+    <Provider store={store}>
+      <div className="">
+        <h1>Search for a package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
