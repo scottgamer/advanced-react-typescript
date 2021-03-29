100cb581ecfc74b771913adc2f96a37aee0543e0
rmunoz <richosojason@msn.com>
2021-03-28 23:22:31 -0500

    added bundler component

---
+++ jbook/src/components/preview.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef } from "react";
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event)=> {
+            try{
+              eval(event.data)
+            } catch(error){
+              const root = document.querySelector("#root");
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error:</h4>' + error + '</div>';
+              console.error(error);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
+const Preview = ({ code }: PreviewProps): JSX.Element => {
+  const iframe = useRef<any>(null);
+
+  useEffect(() => {
+    if (!iframe.current) {
+      return;
+    }
+    iframe.current.srcdoc = html;
+    iframe.current.contentWindow.postMessage(code, "*");
+  }, [code]);
+
+  return (
+    <iframe
+      ref={iframe}
+      sandbox="allow-scripts"
+      srcDoc={html}
+      title="preview"
+    />
+  );
+};
+
+export default Preview;
