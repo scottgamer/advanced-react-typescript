0597f51bb4550815420c966bc255f8deb43ef3a3
rmunoz <richosojason@msn.com>
2021-04-04 18:52:40 -0500

    added text-editor component

---
+++ jbook/src/components/text-editor.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import MDEditor from "@uiw/react-md-editor";
+
+import "./text-editor.css";
+
+const TextEditor = () => {
+  const [editing, setEditing] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-editor" onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;
