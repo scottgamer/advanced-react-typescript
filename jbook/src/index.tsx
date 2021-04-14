55e6ca8f40f7df5b293518fc0a0e4aa6c3dd84bc
rmunoz <richosojason@msn.com>
2021-04-13 23:17:28 -0500

    added provider to main component

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,16 +1,20 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
+import TextEditor from "./components/text-editor";
+import { store } from "./state";
 
 // import CodeCell from "./components/code-cell";
-import TextEditor from "./components/text-editor";
-
-import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeCell /> */}
+      </div>
+    </Provider>
   );
 };
 
