99b53511f33bf1a17b7d987aea3abfed34bf3a55
rmunoz <richosojason@msn.com>
2021-04-14 22:14:27 -0500

    added null operator

--- jbook/src/state/store.ts
+++ jbook/src/state/store.ts
@@ -1,6 +1,26 @@
 import { applyMiddleware, createStore } from "redux";
 
+// import { ActionType } from "./action-types";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// TODO: just for testing purposes
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: "code",
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: "text",
+//   },
+// });
+
+// console.log(store.getState());
