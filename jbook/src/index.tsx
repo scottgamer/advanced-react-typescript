5e5359c2b98cd665d5ee602bc76677b2ef15d755
rmunoz <richosojason@msn.com>
2021-04-24 13:13:42 -0500

    added cell list component to index component

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,18 +2,13 @@ import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
-import TextEditor from "./components/text-editor";
 import { store } from "./state";
-
-// import CodeCell from "./components/code-cell";
+import CellList from "./components/cell-list";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <TextEditor />
-        {/* <CodeCell /> */}
-      </div>
+      <CellList />
     </Provider>
   );
 };
