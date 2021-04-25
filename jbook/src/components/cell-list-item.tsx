ed120635e301262b0ea833f6d9262f4842374720
rmunoz <richosojason@msn.com>
2021-04-25 11:52:30 -0500

    added actionBar to cellListItem

--- jbook/src/components/cell-list-item.tsx
+++ jbook/src/components/cell-list-item.tsx
@@ -3,6 +3,7 @@ import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
 import ActionBar from "./action-bar";
+import "./cell-list-item.css";
 
 interface Props {
   cell: Cell;
@@ -11,17 +12,24 @@ interface Props {
 const CellListItem = ({ cell }: Props): JSX.Element => {
   let child: JSX.Element;
   if (cell.type === "code") {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
 
-  return (
-    <div>
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
