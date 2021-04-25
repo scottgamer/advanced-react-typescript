5a3e65b063067eecfbe45394c1c8d2b976e6a2d0
rmunoz <richosojason@msn.com>
2021-04-24 19:36:21 -0500

    added condition for CellListItem component

--- jbook/src/components/cell-list-item.tsx
+++ jbook/src/components/cell-list-item.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Cell } from "../state";
+import CodeCell from "./code-cell";
+import TextEditor from "./text-editor";
 
-const CellListItem: React.FC = (): JSX.Element => {
-  return <div></div>;
+interface Props {
+  cell: Cell;
+}
+
+const CellListItem = ({ cell }: Props): JSX.Element => {
+  let child: JSX.Element;
+  if (cell.type === "code") {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
