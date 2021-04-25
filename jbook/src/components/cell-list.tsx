685916130cfc3d04b829834374c2928b7cf5f35e
rmunoz <richosojason@msn.com>
2021-04-24 19:36:55 -0500

    added useTyppedSelector to cellList component

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import CellListItem from "./cell-list-item";
+
 const CellList: React.FC = (): JSX.Element => {
-  useTypedSelector((state) => state);
-  return <div>Cell list</div>;
+  const cells = useTypedSelector(({ cells }) => {
+    if (cells) {
+      return cells.order.map((id) => cells.data[id]);
+    }
+    return;
+  });
+
+  const renderedCells =
+    cells && cells.map((cell) => <CellListItem key={cell.id} cell={cell} />);
+
+  return <div>{renderedCells}</div>;
 };
 
 export default CellList;
