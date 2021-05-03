7b9121296ba355ecd0ff907886016d4b244f3e64
rmunoz <richosojason@msn.com>
2021-05-02 22:19:31 -0500

    added create bundle reducer

--- jbook/src/state/action-creators/index.ts
+++ jbook/src/state/action-creators/index.ts
@@ -1,13 +1,17 @@
+import { Dispatch } from "redux";
 import {
   DeleteCellAction,
   Direction,
   InsertCellAfterAction,
   MoveCellAction,
   UpdateCellAction,
+  Action,
 } from "../actions";
 
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
+
+import bundle from "../../bundler";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -43,5 +47,29 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: {
+          code: result.code,
+          err: result.error,
+        },
+      },
+    });
   };
 };
