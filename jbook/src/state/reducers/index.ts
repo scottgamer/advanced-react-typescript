908a11f0d0108efb3bc95459116c532038b81296
rmunoz <richosojason@msn.com>
2021-05-02 22:19:57 -0500

    added bundles reducer to reducers index

--- jbook/src/state/reducers/index.ts
+++ jbook/src/state/reducers/index.ts
@@ -1,8 +1,10 @@
-import cellsReducer from "./cells-reducer";
 import { combineReducers } from "redux";
+import cellsReducer from "./cells-reducer";
+import bundlesReducer from "./bundles-reducer";
 
 const reducers = combineReducers({
   cells: cellsReducer,
+  bundle: bundlesReducer,
 });
 
 export default reducers;
