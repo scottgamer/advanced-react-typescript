30a6ed9c3f748ce33fca1d9aad0f8e8cfc5503c0
rmunoz <richosojason@msn.com>
2021-05-09 23:22:33 -0500

    added jsx config to esbuild

--- jbook/src/bundler/index.ts
+++ jbook/src/bundler/index.ts
@@ -23,6 +23,8 @@ const bundler = async (rawCode: string) => {
         "process.env.NODE_ENV": '"production"',
         global: "window",
       },
+      jsxFactory: "_React.createElement",
+      jsxFragment: "_React.Fragment",
     });
 
     return { code: result.outputFiles[0].text, error: "" };
