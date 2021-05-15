5ef7304a301c7b4a0250a25fd5a7f274e424383b
rmunoz <richosojason@msn.com>
2021-05-14 22:09:46 -0500

    added promise to express

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,3 +1,9 @@
-export default () => {
-  console.log("server is listening");
+import express from "express";
+
+export const serve = (port: number, filename: string, dir: string) => {
+  const app = express();
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
 };
